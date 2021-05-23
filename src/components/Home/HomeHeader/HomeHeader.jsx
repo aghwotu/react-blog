ee4f5fc441f0a3fb70bc9e011b2548fbe366a464
Aghwotu Ovuoke <aghwotuovuoke@gmail.com>
2021-05-23 02:49:24 +0100

    style(<a>tags): fix anchor tag styles

--- src/components/Home/HomeHeader/HomeHeader.jsx
+++ src/components/Home/HomeHeader/HomeHeader.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import classNames from 'classnames';
+// eslint-disable-next-line no-unused-vars
 import Link from '../../_ui/Link/Link';
 import WatercolorCanvas from '../../WatercolorCanvas/WatercolorCanvas';
 import Pdf from '../../../CV.pdf';
@@ -22,23 +23,35 @@ class HomeHeader extends Component {
 
             <div className="HomeHeader__content__text__description">
               I'm a frontend developer. a doctoral student studying{' '}
-              <Link to="https://cmj.umaine.edu/">
+              <a
+                href="https://cmj.umaine.edu/"
+                target="_blank"
+                rel="noreferrer"
+              >
                 Environmental Communication
-              </Link>{' '}
+              </a>{' '}
               and{' '}
-              <Link to="https://umaine.edu/womensgenderandsexualitystudies/">
+              <a
+                href="https://umaine.edu/womensgenderandsexualitystudies/"
+                target="_blank"
+                rel="noreferrer"
+              >
                 Women’s, Gender & Sexuality Studies
-              </Link>{' '}
-              at the <Link to="University of Maine">University of Maine</Link> .
-              I also teach courses in communication and journalism. My current
+              </a>{' '}
+              at the <a href="University of Maine">University of Maine</a> . I
+              also teach courses in communication and journalism. My current
               work explores affective quality of potent collectivity in digital
               activism and methodology of digital humanities with R. Most of my
               publications and working papers are available on{' '}
-              <Link to="https://papers.ssrn.com/sol3/cf_dev/AbsByAuth.cfm?per_id=3532935">
+              <a
+                href="https://papers.ssrn.com/sol3/cf_dev/AbsByAuth.cfm?per_id=3532935"
+                target="_blank"
+                rel="noreferrer"
+              >
                 SSRN
-              </Link>
+              </a>
               , along with my{' '}
-              <Link to="https://hartbullock.com/course/cse-138/"></Link> course
+              {/* <Link href="https://hartbullock.com/course/cse-138/"></Link> course */}
               offerings on this website. My{' '}
               <a href={Pdf} target="_blank" rel="noreferrer">
                 Curriculum Vitae{' '}
@@ -47,13 +60,23 @@ class HomeHeader extends Component {
               <br />
               <br />
               You can find me on{' '}
-              <Link to="https://twitter.com/HartBullock">Twitter</Link> connect
-              via{' '}
-              <Link to="https://www.linkedin.com/in/hartbullock/">
+              <a
+                href="https://twitter.com/HartBullock"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Twitter
+              </a>{' '}
+              connect via{' '}
+              <a
+                href="https://www.linkedin.com/in/hartbullock/"
+                target="_blank"
+                rel="noreferrer"
+              >
                 LinkedIn
-              </Link>{' '}
+              </a>{' '}
               , or reach out by{' '}
-              <Link href="mailto:minglei.zhang@maine.edu">email</Link>.
+              <a href="mailto:minglei.zhang@maine.edu">email</a>.
             </div>
           </div>
         </div>
